8d377f0fda7e9646ebe69f0ab2d7c8dee9740bb2
Will Hamilton <ishi12@icloud.com>
2020-09-11 18:31:23 -0400

    func (c SpotifyClient) GetArtistAlbums(id string) (albums ArtistAlbums, e error)

--- spotify/spotify.go
+++ spotify/spotify.go
@@ -102,81 +102,7 @@ type SpotifyAlbum struct {
 }
 
 type SpotifyAlbums struct {
-	Albums []struct {
-		AlbumType string `json:"album_type"`
-		Artists   []struct {
-			ExternalUrls struct {
-				Spotify string `json:"spotify"`
-			} `json:"external_urls"`
-			Href string `json:"href"`
-			ID   string `json:"id"`
-			Name string `json:"name"`
-			Type string `json:"type"`
-			URI  string `json:"uri"`
-		} `json:"artists"`
-		AvailableMarkets []string `json:"available_markets"`
-		Copyrights       []struct {
-			Text string `json:"text"`
-			Type string `json:"type"`
-		} `json:"copyrights"`
-		ExternalIds struct {
-			Upc string `json:"upc"`
-		} `json:"external_ids"`
-		ExternalUrls struct {
-			Spotify string `json:"spotify"`
-		} `json:"external_urls"`
-		Genres []interface{} `json:"genres"`
-		Href   string        `json:"href"`
-		ID     string        `json:"id"`
-		Images []struct {
-			Height int64  `json:"height"`
-			URL    string `json:"url"`
-			Width  int64  `json:"width"`
-		} `json:"images"`
-		Label                string `json:"label"`
-		Name                 string `json:"name"`
-		Popularity           int64  `json:"popularity"`
-		ReleaseDate          string `json:"release_date"`
-		ReleaseDatePrecision string `json:"release_date_precision"`
-		TotalTracks          int64  `json:"total_tracks"`
-		Tracks               struct {
-			Href  string `json:"href"`
-			Items []struct {
-				Artists []struct {
-					ExternalUrls struct {
-						Spotify string `json:"spotify"`
-					} `json:"external_urls"`
-					Href string `json:"href"`
-					ID   string `json:"id"`
-					Name string `json:"name"`
-					Type string `json:"type"`
-					URI  string `json:"uri"`
-				} `json:"artists"`
-				AvailableMarkets []string `json:"available_markets"`
-				DiscNumber       int64    `json:"disc_number"`
-				DurationMs       int64    `json:"duration_ms"`
-				Explicit         bool     `json:"explicit"`
-				ExternalUrls     struct {
-					Spotify string `json:"spotify"`
-				} `json:"external_urls"`
-				Href        string `json:"href"`
-				ID          string `json:"id"`
-				IsLocal     bool   `json:"is_local"`
-				Name        string `json:"name"`
-				PreviewURL  string `json:"preview_url"`
-				TrackNumber int64  `json:"track_number"`
-				Type        string `json:"type"`
-				URI         string `json:"uri"`
-			} `json:"items"`
-			Limit    int64       `json:"limit"`
-			Next     interface{} `json:"next"`
-			Offset   int64       `json:"offset"`
-			Previous interface{} `json:"previous"`
-			Total    int64       `json:"total"`
-		} `json:"tracks"`
-		Type string `json:"type"`
-		URI  string `json:"uri"`
-	} `json:"albums"`
+	Albums []SpotifyAlbum
 }
 
 
@@ -232,6 +158,10 @@ type SpotifyClient struct {
 	AccessToken string
 }
 
+//
+// ALBUMS
+//
+
 func (c *SpotifyClient) Authenticate() error {
 	decodedHeader := c.ClientId + ":" + c.ApiSecret
 	encodedHeader := "Basic " + base64.StdEncoding.EncodeToString([]byte(decodedHeader))
@@ -281,4 +211,57 @@ func (c SpotifyClient) GetAlbums(ids []string) (albums SpotifyAlbums, e error) {
 	} else {
 		return albums, errors.New("Maximum of 20 IDs Per Call")
 	}
+}
+
+//
+// ARISTS
+//
+
+type ArtistAlbums struct {
+	Href  string `json:"href"`
+	Items []struct {
+		AlbumGroup string `json:"album_group"`
+		AlbumType  string `json:"album_type"`
+		Artists    []struct {
+			ExternalUrls struct {
+				Spotify string `json:"spotify"`
+			} `json:"external_urls"`
+			Href string `json:"href"`
+			ID   string `json:"id"`
+			Name string `json:"name"`
+			Type string `json:"type"`
+			URI  string `json:"uri"`
+		} `json:"artists"`
+		AvailableMarkets []string `json:"available_markets"`
+		ExternalUrls     struct {
+			Spotify string `json:"spotify"`
+		} `json:"external_urls"`
+		Href   string `json:"href"`
+		ID     string `json:"id"`
+		Images []struct {
+			Height int64  `json:"height"`
+			URL    string `json:"url"`
+			Width  int64  `json:"width"`
+		} `json:"images"`
+		Name                 string `json:"name"`
+		ReleaseDate          string `json:"release_date"`
+		ReleaseDatePrecision string `json:"release_date_precision"`
+		TotalTracks          int64  `json:"total_tracks"`
+		Type                 string `json:"type"`
+		URI                  string `json:"uri"`
+	} `json:"items"`
+	Limit    int64       `json:"limit"`
+	Next     string      `json:"next"`
+	Offset   int64       `json:"offset"`
+	Previous interface{} `json:"previous"`
+	Total    int64       `json:"total"`
+}
+
+func (c SpotifyClient) GetArtistAlbums(id string) (albums ArtistAlbums, e error) {
+	resp, err := apiRequest("https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums", "GET", []byte{}, c.AccessToken)
+	if err != nil { return albums,err}
+	fmt.Println(string(resp.response))
+	err2 := json.Unmarshal(resp.response, &albums)
+	if err2 != nil { return albums, err2 }
+	return albums, nil
 }
